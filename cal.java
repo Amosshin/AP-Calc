9d788b71c9a32f265c342c4dd12bf4832276023a
Amos Shin <amos.shin@vtsd.com>
2023-01-26 07:52:29 -0500

    f

--- cal.java
+++ cal.java
@@ -3,56 +3,68 @@ public class cal {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        double[] array = new double[8];
+        double[] grade = new double[8];
         double[] credit;
         credit = new double[8];
-        for (int k = 0; k < array.length; k++) {
+        for (int k = 0; k < grade.length; k++) {
             int a = k + 1;
             System.out.print("Enter your grade for period" + a + "--> ");
-            array[k] = input.nextDouble();
+            grade[k] = input.nextDouble();
             double AP = 1.09;
             double Honors = 1.06;
             double ACP = 1.02;
             double CP = 1;
-            System.out.print("Enter the weighted value( AP as 1.09, Honors as 1.06, ACP as 1.02, CP as 1 -->  ");
-            double w = input.nextDouble();
+            System.out.print("Enter the weighted value( CP as 1, ACP as 2, Honors as 3, AP as 4) -->  ");
+            int w = input.nextInt();
             System.out.print("Enter the credit ");
             credit[k] = input.nextInt();
-            if (w == 1.09) {
-                array[k] = array[k] * AP * credit[k];
+            if (w == 4) {
+               grade[k] = grade[k] * AP * credit[k];
             }
-            if (w == 1.06) {
-                array[k] = array[k] * Honors * credit[k];
+            if (w == 3) {
+               grade[k] = grade[k] * Honors * credit[k];
             }
-            if (w == 1.02) {
-                array[k] = array[k] * ACP * credit[k];
+            if (w == 2) {
+               grade[k] = grade[k] * ACP * credit[k];
             }
             if (w == 1) {
-                array[k] = array[k] * CP * credit[k];
-            } else {
-                System.out.println("error number, please enter the correct weighted value");
-                array[0] = 333;
-                break;
+                grade[k] =grade[k] * CP * credit[k];
             }
-
+           if(w >= 5) {
+                System.out.println("error number, please enter the correct weighted value");
+               grade[0] = 333;
+              break;
+           }
         }
-
         double tc = 0;
         for (int g = 0; g < credit.length; g++) {
             tc += credit[g];
         }
-
-
         double sum = 0;
-        for (int t = 0; t < array.length; t++) {
-            sum += array[t];
+        for (int t = 0; t < grade.length; t++) {
+            sum += grade[t];
         }
        double gpa = sum / tc;
-        if (array[0] != 333) {
+        double RoundGpa = gpa * 100;
+        if (grade[0] != 333) {
             System.out.print("Final gpa: " + gpa);
-
+            gpa = Math.round(RoundGpa)/100;
+            System.out.print("Rounded gpa: " + gpa);
         }
+        System.out.print("Enter your SAT score");
+        int SAT = input.nextInt();
+
+
+
+
+
     }
+
+
+
+
+
+
 }
 
 
