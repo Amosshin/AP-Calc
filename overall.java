8d378166f178871f65cff6b3a271d32c4c12105f
Amos Shin <amos.shin@vtsd.com>
2023-02-07 07:53:01 -0500

    f

--- overall.java
+++ overall.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
-
 public class overall {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("What gpa scale do you use?");
+        System.out.println("Type 1 for the 100scale and 2 for the 4.0 scale");
+        int choice = input.nextInt();
+        switch (choice) {
+            case 1:
+                cal.StyleOne();break;
+            case 2:
+                cal.StyleTwo();break;
+            default: System.out.println("Error, please try it again");
 
 
+        }
 
-
-
-
-
-
-
+        }
     }
-
-
-
-}
